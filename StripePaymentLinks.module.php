901b87d16b0a2193686d9a0dbe9a8d170db136df
frameless Media <office@frameless.at>
2025-11-11 20:13:16 +0100

    bumped version to 1.0.14

--- StripePaymentLinks.module.php
+++ StripePaymentLinks.module.php
@@ -34,7 +34,7 @@ class StripePaymentLinks extends WireData implements Module, ConfigurableModule 
 	public static function getModuleInfo(): array {
 		return [
 			'title'       => 'StripePaymentLinks',
-			'version'     => '1.0.13', 
+			'version'     => '1.0.14', 
 			'summary'     => 'Stripe payment-link redirects, user/purchases, magic link, mails, modals.',
 			'author'      => 'frameless Media',
 			'autoload'    => true,
